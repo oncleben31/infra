5df969d44bb704ae5fda735728f22ad6a6a42cd4
oncleben31 <oncleben31@users.noreply.github.com>
2021-02-16 23:06:51 +0100

    Add command in aliases

--- roles/oncleben31-sombra/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
+++ roles/oncleben31-sombra/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,4 @@
 alias dcp="docker-compose -f ~/docker-compose.yml "
 alias dcporph="docker-compose -f ~/docker-compose.yml --remove-orphans "
 alias dprune="docker image prune"
+alias dcips=$'docker inspect -f \'{{.Name}}-{{range  $k, $v := .NetworkSettings.Networks}}{{$k}}-{{.IPAddress}} {{end}}-{{range $k, $v := .NetworkSettings.Ports}}{{ if not $v }}{{$k}} {{end}}{{end}} -{{range $k, $v := .NetworkSettings.Ports}}{{ if $v }}{{$k}} => {{range . }}{{ .HostIp}}:{{.HostPort}}{{end}}{{end}} {{end}}\' $(docker ps -aq) | column -t -s-'
