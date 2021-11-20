7dc21bda5a76df1e3eae2ec5512570e3e03fc76b
oncleben31 <oncleben31@users.noreply.github.com>
2021-11-20 08:46:41 +0100

    Fix alias

--- roles/oncleben31-sombra/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
+++ roles/oncleben31-sombra/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
@@ -1,5 +1,5 @@
 alias dcp="docker-compose -f ~/docker-compose.yml "
-alias dcporph="docker-compose -f ~/docker-compose.yml --remove-orphans "
+alias dcporph="docker-compose -f ~/docker-compose.yml up -d --remove-orphans"
 alias dprune="docker image prune"
 alias dcips=$'docker inspect -f \'{{.Name}}-{{range  $k, $v := .NetworkSettings.Networks}}{{$k}}-{{.IPAddress}} {{end}}-{{range $k, $v := .NetworkSettings.Ports}}{{ if not $v }}{{$k}} {{end}}{{end}} -{{range $k, $v := .NetworkSettings.Ports}}{{ if $v }}{{$k}} => {{range . }}{{ .HostIp}}:{{.HostPort}}{{end}}{{end}} {{end}}\' $(docker ps -aq) | column -t -s-'
 alias ctop="docker run --name ctop  -ti --rm --volume /var/run/docker.sock:/var/run/docker.sock:ro quay.io/vektorlab/ctop:latest "
